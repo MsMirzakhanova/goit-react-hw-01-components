219424d6fcdb406e8af6d560e0a011922de7356f
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-12-11 19:26:23 +0200

    1taskCss

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -16,15 +16,15 @@ export const Profile = ({ username, tag, location, avatar, followers, views, lik
   </div>
 
   <ul className={css.stats}>
-    <li>
+    <li className={css.item}>
         <span className={css.label}>Followers</span>
         <span className={css.quantity}>{followers}</span>
     </li>
-    <li>
+    <li className={css.item}>
         <span className={css.label}>Views</span>
         <span className={css.quantity}>{views}</span>
     </li>
-    <li>
+    <li className={css.item}>
         <span className={css.label}>Likes</span>
         <span className={css.quantity}>{likes}</span>
     </li>
